2c9e40f52bf451d05495e1d1b7308bfc6894093c
AdityaGoel01 <goeladitya101@gmail.com>
2021-02-18 20:16:01 +0530

    First Commit

---
+++ TikTok Backend/data.js
@@ -0,0 +1,21 @@
+const data = [
+    {
+        "url":"https://cdn3.sharechat.com/160945428e256f7_1609454800511_v_750.mp4" ,
+        "channel": "aditya01", 
+        "description": "YOO this WORKS!", 
+        "song": "GangLand" ,
+        "likes": 100 ,
+        "messages": 250,
+        "shares": 12
+    },
+    {
+        "url":"https://cdn3.sharechat.com/160945428e256f7_1609454800511_v_750.mp4" ,
+        "channel": "aditya02", 
+        "description": "YOO this WORKS!", 
+        "song": "Illegal Weapons" ,
+        "likes": 10101 ,
+        "messages": 1250,
+        "shares": 121
+    }
+];
+export default data;
